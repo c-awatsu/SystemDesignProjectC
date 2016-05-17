364cfd5750f84cb54119f8dfb5568e12d589a1ee
c-awatsu <cist.b213.c.awatsu@gmai.com>
2016-05-17 10:53:20 +0900

    SignInPage.javaを作成

--- src/main/java/com/SysC/signIn/SignInPage.java
+++ src/main/java/com/SysC/signIn/SignInPage.java
@@ -1,5 +1,11 @@
 package com.SysC.signIn;
 
-public class SignInPage {
 
+public class SignInPage extends AbstractSignInPage{
+
+	private static final long serialVersionUID = -4239028352453322459L;
+
+	public SignInPage(){
+
+	}
 }
