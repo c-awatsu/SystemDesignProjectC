468f1c1d723ddc58d6fd1748988c1716d50be152
shun-nagata <cist.b214.s.nagata@gmail.com>
2016-05-17 14:27:54 +0900

    SignInPageの作成

--- src/main/java/com/SysC/WicketApplication.java
+++ src/main/java/com/SysC/WicketApplication.java
@@ -4,7 +4,7 @@ import org.apache.wicket.guice.GuiceComponentInjector;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-import com.SysC.signIn.AbstractSignInPage;
+import com.SysC.signIn.SignInPage;
 
 /**
  * Application object for your web application.
@@ -20,7 +20,7 @@ public class WicketApplication extends WebApplication
 	@Override
 	public Class<? extends WebPage> getHomePage()
 	{
-		return AbstractSignInPage.class;
+		return SignInPage.class;
 	}
 
 	/**
--- src/main/java/com/SysC/signIn/SignInPage.java
+++ src/main/java/com/SysC/signIn/SignInPage.java
@@ -1,11 +1,45 @@
 package com.SysC.signIn;
 
 
-public class SignInPage extends AbstractSignInPage{
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
-	private static final long serialVersionUID = -4239028352453322459L;
 
-	public SignInPage(){
+
+
+public class SignInPage extends AbstractSignInPage {
+
+	private static final long serialVersionUID = 1L;
+
+	public SignInPage() {
+
+		final TextField<String> accountField = new TextField<String>(
+			"accountName",
+			new Model<String>(""));
+
+		final PasswordTextField passField = new PasswordTextField(
+				"passphrase",
+				new Model<String>(""));
+
+
+		Form<Void> submitForm = new Form<Void>("form"){
+
+		@Override
+		protected void onSubmit(){
+			String accountValue = accountField.getModelObject();
+			setDefaultModelObject(accountValue);
+
+			String passValue = passField.getModelObject();
+			setDefaultModelObject(passValue);
+
+		}
+		};
+
+		submitForm.add(accountField);
+		submitForm.add(passField);
+		add(submitForm);
 
 	}
 }
