290971f5718524e1cd37ccfa78cad59f1b9a3d6d
esukepu <esukepu@esukepu-no-MacBook-Air.local>
2016-07-15 14:15:39 +0900

    TA待機ページのnullチェック

--- src/main/java/com/SysC/dao/BusinessRepository.java
+++ src/main/java/com/SysC/dao/BusinessRepository.java
@@ -11,49 +11,49 @@ import com.SysC.JDBCUtill;
 import com.SysC.bean.BusinessItem;
 import com.SysC.bean.LectureItem;
 
-public class BusinessRepository implements IBusinessRepository{
+public class BusinessRepository implements IBusinessRepository {
 
 	@Override
-	public int insertBusiness(String business,String other,String col,String row){
+	public int insertBusiness(String business, String other, String col, String row) {
 		int result = 0;
 		String sql = "insert into call_log(business,other,col_position,row_position) values(?,?,?,?)";
-		try(Connection conn = JDBCUtill.getConnection()){
-			try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+		try (Connection conn = JDBCUtill.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 				pstmt.setString(1, business);
 				pstmt.setString(2, other);
 				pstmt.setString(3, col);
 				pstmt.setString(4, row);
 				result = pstmt.executeUpdate();
 			}
-		}catch(SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	//call_logテーブルのデータベースにアクセスして値を取り出す
+
+	// call_logテーブルのデータベースにアクセスして値を取り出す
 	@Override
-	public List<BusinessItem> selectBusinessItems(){
+	public List<BusinessItem> selectBusinessItems() {
 		List<BusinessItem> businessItems = new ArrayList<>();
 		String sql = "select business,other,col_position,row_position from call_log";
-		try(Connection conn = JDBCUtill.getConnection()){
-			try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+		try (Connection conn = JDBCUtill.getConnection()) {
+			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
 				ResultSet result = pstmt.executeQuery();
-				while(result.next()){
-						BusinessItem item = new BusinessItem();
-						item.setBusiness(result.getString(1));
-						item.setOther(result.getString(2));
-						item.setCol(result.getString(3));
-						item.setRow(result.getString(4));
-						
-						businessItems.add(item);
+				while (result.next()) {
+					BusinessItem item = new BusinessItem();
+					item.setBusiness(result.getString(1));
+					item.setOther(result.getString(2));
+					item.setCol(result.getString(3));
+					item.setRow(result.getString(4));
+
+					businessItems.add(item);
 				}
 			}
-		}catch(SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
 		return businessItems;
--- src/main/java/com/SysC/signed/questionnaire/WatingQuestionPage.java
+++ src/main/java/com/SysC/signed/questionnaire/WatingQuestionPage.java
@@ -12,70 +12,74 @@ import com.SysC.service.IBusinessService;
 import com.SysC.signed.AbstractSignedPage;
 import com.google.inject.Inject;
 
-public class WatingQuestionPage extends AbstractSignedPage{
+public class WatingQuestionPage extends AbstractSignedPage {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	private IBusinessService businessService;
-	
-	public WatingQuestionPage(){
-		//用件表示リスト
+
+	public WatingQuestionPage() {
+		// 用件表示リスト
 		List<BusinessItem> businessList = businessService.selectBusinessItems();
-		add(new ListView<BusinessItem>("businessListView",businessList){
+		add(new ListView<BusinessItem>("businessListView", businessList) {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
-			protected void populateItem(ListItem<BusinessItem> item){
-				
-				item.add(new Label("business","用件：" + businessList.get(item.getIndex()).getBusiness().getLabel()));
-				item.add(new Label("other","その他：" + businessList.get(item.getIndex()).getOther()));
-				item.add(new Label("col","席番号：" + businessList.get(item.getIndex()).getCol().getLabel()));
-				item.add(new Label("row",businessList.get(item.getIndex()).getRow().getLabel()));
-				
+			protected void populateItem(ListItem<BusinessItem> item) {
+
+				item.add(new Label("business", "用件：" + businessList.get(item.getIndex()).getBusiness().getLabel()));
+				if (businessList.get(item.getIndex()).getOther() == null) {
+					item.add(new Label("other", "詳細：" + "          "));
+				} else {
+					item.add(new Label("other", "詳細：" + businessList.get(item.getIndex()).getOther()));
+				}
+				item.add(new Label("col", "席番号：" + businessList.get(item.getIndex()).getCol().getLabel()));
+				item.add(new Label("row", businessList.get(item.getIndex()).getRow().getLabel()));
+
 			}
 		});
 
-//		Button correspondenceButton = new Button("correspondence"){
-//			private static final long serialVersionUID = -3717812991518096008L;
-//
-//			@Override
-//			public void onSubmit(){
-//			}
-//		};
-//
-//		Button correspondenceNowButton = new Button("correspondenceNow"){
-//			private static final long serialVersionUID = -8189048307803983272L;
-//
-//			@Override
-//			public void onSubmit(){
-//			}
-//		};
-//
-//		Button completeButton = new Button("complete"){
-//			private static final long serialVersionUID = 5980279017721503734L;
-//
-//			@Override
-//			public void onSubmit(){
-//			}
-//		};
-//
-//		Button finishButton = new Button("finish"){
-//			private static final long serialVersionUID = 7192402134540635892L;
-//
-//			@Override
-//			public void onSubmit(){
-//			}
-//		};
-//
-//		//授業終了ボタン
-//		Button lectureFinishButton = new Button("lectureFinish"){
-//			private static final long serialVersionUID = -8609536677246760148L;
-//
-//			@Override
-//			public void onSubmit(){
-//			}
-//		};
+		// Button correspondenceButton = new Button("correspondence"){
+		// private static final long serialVersionUID = -3717812991518096008L;
+		//
+		// @Override
+		// public void onSubmit(){
+		// }
+		// };
+		//
+		// Button correspondenceNowButton = new Button("correspondenceNow"){
+		// private static final long serialVersionUID = -8189048307803983272L;
+		//
+		// @Override
+		// public void onSubmit(){
+		// }
+		// };
+		//
+		// Button completeButton = new Button("complete"){
+		// private static final long serialVersionUID = 5980279017721503734L;
+		//
+		// @Override
+		// public void onSubmit(){
+		// }
+		// };
+		//
+		// Button finishButton = new Button("finish"){
+		// private static final long serialVersionUID = 7192402134540635892L;
+		//
+		// @Override
+		// public void onSubmit(){
+		// }
+		// };
+		//
+		// //授業終了ボタン
+		// Button lectureFinishButton = new Button("lectureFinish"){
+		// private static final long serialVersionUID = -8609536677246760148L;
+		//
+		// @Override
+		// public void onSubmit(){
+		// }
+		// };
 	}
 
 }
