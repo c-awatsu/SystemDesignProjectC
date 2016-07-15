b0d853a8c6a66210f9370b2ff9a34405364add0d
Owner <owner@imac.local>
2016-07-15 18:13:05 +0900

    教員ページの表示修正

--- src/main/java/com/SysC/signed/questionnaire/CollectionQuestionairePage.java
+++ src/main/java/com/SysC/signed/questionnaire/CollectionQuestionairePage.java
@@ -6,7 +6,9 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 
+import com.SysC.bean.CommentItem;
 import com.SysC.bean.QuestionnaireItem;
+import com.SysC.service.ICommentService;
 import com.SysC.service.IQuestionaireService;
 import com.SysC.signed.AbstractSignedPage;
 import com.google.inject.Inject;
@@ -15,12 +17,13 @@ public class CollectionQuestionairePage extends AbstractSignedPage{
 	private static final long serialVersionUID = 1L;
 
 	@Inject
-
 	private IQuestionaireService questionnaireService;
+	@Inject
+	private ICommentService commentService;
 
 	public CollectionQuestionairePage() {
 
-
+		// わからないボタンが押された回数を表示
 		List<QuestionnaireItem> questionnareList = questionnaireService.selectQuestionnaireItems();
 		add(new ListView<QuestionnaireItem>("questionnaireListView",questionnareList){
 
@@ -28,12 +31,22 @@ public class CollectionQuestionairePage extends AbstractSignedPage{
 
 			@Override
 			protected void populateItem(ListItem<QuestionnaireItem> item){
-				item.add(new Label("no",+ questionnareList.get(item.getIndex()).getNo()));
+				item.add(new Label("no","わからないボタンクリック数：" + questionnareList.get(item.getIndex()).getNo()));
 			}
-
-
 		});
+		
+		// 質問の表示
+		List<CommentItem> commentList = commentService.selectCommentItems();
 
+		add(new ListView<CommentItem>("commentListView",commentList){
+
+			private static final long serialVersionUID = -5561173865981006135L;
+
+			@Override
+			protected void populateItem(ListItem<CommentItem> item){
+				item.add(new Label("comment","用件：" + commentList.get(item.getIndex()).getComment()));
+			}
+		});
 	}
 }
 
